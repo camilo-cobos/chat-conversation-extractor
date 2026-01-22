d581435f3662336b01943dc8b970ea64f76e13c6
camilo-cobos <jccobosc@udistrital.edu.co>
2026-01-21 21:54:42 -0500

    Update file_generator.py

--- app/file_generator.py
+++ app/file_generator.py
@@ -1,1 +1,14 @@
+from docx import Document
+
+def generar_txt(mensajes: list[str], ruta: str):
+    with open(ruta, "w", encoding="utf-8") as f:
+        for mensaje in mensajes:
+            f.write(mensaje + "\n\n")
+
+
+def generar_docx(mensajes: list[str], ruta: str):
+    doc = Document()
+    for mensaje in mensajes:
+        doc.add_paragraph(mensaje)
+    doc.save(ruta)
 
